8dc8d60d8b4eecd0142c3cb07a5766a58064c39f
NiTro005 <nitro2005002@gmail.com>
2025-02-22 18:17:42 +0300

    realise Matrix filter class

--- Lab_1/Lab_1/MatrixFilter.cs
+++ Lab_1/Lab_1/MatrixFilter.cs
@@ -14,7 +14,23 @@ namespace Lab_1
         protected MatrixFilter(float[,] kernel) { this.kernel = kernel; }
         protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
         {
-            throw new NotImplementedException();
+            int radiusX = kernel.GetLength(0) / 2;
+            int radiusY = kernel.GetLength(1) / 2;
+            float resultR = 0;
+            float resultG = 0;
+            float resultB = 0;
+            for (int l = -radiusX; l <= radiusX; l++) {
+                for (int k = -radiusY; k <= radiusY; k++) {
+                    int Xneib = Clamp(x + k, 0, sourceImage.Width - 1);
+                    int Yneib = Clamp(y + l, 0, sourceImage.Height - 1);
+                    Color neib_color = sourceImage.GetPixel(Xneib, Yneib);
+                    resultR += neib_color.R * kernel[l + radiusX, k + radiusY];
+                    resultG += neib_color.G * kernel[l + radiusX, k + radiusY];
+                    resultB += neib_color.B * kernel[l + radiusX, k + radiusY];
+                }
+            }
+            return Color.FromArgb(Clamp((int)resultR, 0, 255),
+                Clamp((int)resultG, 0, 255), Clamp((int)resultB, 0, 255));
         }
 
     }
